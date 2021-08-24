0cd90e426f65e84846ba7d5446e75724decbf5f6
zak <codedpills@gmail.com>
2021-08-24 20:11:36 +0000

    add searchBar

--- src/components/header/SearchBar.jsx
+++ src/components/header/SearchBar.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { ReactComponent as SearchIcon } from "../../assets/icons/search.svg";
 
 const SearchBar = () => {
-  return <div className="search-bar">search</div>;
+  const [showInput, setShowInput] = useState(false);
+  return (
+    <div
+      className="search-bar"
+      onClick={(e) => {
+        setShowInput(true);
+      }}
+      onBlur={(e) => setShowInput(false)}
+    >
+      {showInput ? (
+        <input
+          type="text"
+          placeholder="Search"
+          autoFocus
+          className="search-bar__input"
+        />
+      ) : (
+        <button className="search-bar__button">
+          <SearchIcon /> Search
+        </button>
+      )}
+    </div>
+  );
 };
 
 export default SearchBar;
