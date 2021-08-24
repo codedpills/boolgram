f5e49eaf1e96e2197bdcc8fdc75bcb99ec6e9046
zak <codedpills@gmail.com>
2021-08-24 22:51:59 +0000

    add SideFeed

--- src/components/feed/SideFeed.jsx
+++ src/components/feed/SideFeed.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import Profile from '../profile/Profile';
+import Suggestions from '../suggestions/Suggestions';
+import Footer from '../footer/Footer';
+
 const SideFeed = () => {
     return (
         <section className="side-feed">
-            SideFeed
+            <Profile />
+            <Suggestions />
+            <Footer />
         </section>
     );
 }
