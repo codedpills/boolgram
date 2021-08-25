ec59081f3e940051ff6498677bf4651891964659
zak <codedpills@gmail.com>
2021-08-25 10:45:33 +0000

    render dummy posts

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
+
+import PostHeader from "./PostHeader";
+import PostBody from "./PostBody";
+import PostFooter from "./PostFooter";
+
+import zak from "../../assets/images/zak.png";
+
+import "./posts.css";
 
 const Posts = () => {
-    return (
-        <div>
-            Posts
-        </div>
-    );
-}
+  return (
+    <section className="posts">
+      <PostHeader imageUrl={zak} InstaHandle="zak_sheve2" />
+      <PostBody />
+      <PostFooter />
+    </section>
+  );
+};
 
 export default Posts;
