0f43a267774bef4f57d20b3469f3fd4bdce91426
zak <codedpills@gmail.com>
2021-08-25 00:05:33 +0000

    display dummy stories

--- src/components/stories/Stories.jsx
+++ src/components/stories/Stories.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 
+import StoryItem from './StoryItem';
+
 import './stories.css';
+
+import zak from '../../assets/images/zak.png'
 
 const Stories = () => {
     return (
-        <div>
-            Stories
-        </div>
+        <section className="stories">
+            <StoryItem imageUrl={zak} />
+            <StoryItem imageUrl={zak} />
+            <StoryItem imageUrl={zak} />
+            <StoryItem imageUrl={zak} />
+            <StoryItem imageUrl={zak} />
+            <StoryItem imageUrl={zak} />
+            <StoryItem imageUrl={zak} />
+            <StoryItem imageUrl={zak} />
+            <StoryItem imageUrl={zak} />
+        </section>
     );
 }
 
