346b7935ac1be0b6be5f0d23bdf53aff1d40e86c
zak <codedpills@gmail.com>
2021-08-25 19:57:41 +0000

    render static dummy posts

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -8,11 +8,19 @@ import zak from "../../assets/images/zak.png";
 
 import "./posts.css";
 
+const post = {
+  imageUrl: zak,
+  instaHandle: "zak_sheve2",
+  likes: 22,
+  comment: "watch this space for something awesome",
+  date: "1976-04-19T12:59-0500",
+};
+
 const Posts = () => {
   return (
     <section className="posts">
       <PostHeader imageUrl={zak} InstaHandle="zak_sheve2" />
-      <PostBody />
+      <PostBody post={post} />
       <PostFooter />
     </section>
   );
